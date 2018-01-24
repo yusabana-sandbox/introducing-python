5eb078e567762e03962c639989882dc11b7518db
Yuji Takaesu <taka.esu.user@gmail.com>
2018-01-24 22:50:30 +0900

    add chapter3, years sample

--- 0answer/3/0years.py
+++ 0answer/3/0years.py
@@ -1,3 +1,18 @@
 years_list = [1985, 1986, 1987, 1988, 1989, 1990]
 
 print(years_list)
+
+print(years_list[3])
+
+print(years_list[-1])
+
+
+things = ["mozzarella", "cinderella", "salmonella"]
+
+print([thing.capitalize() for thing in things])
+print(things)
+
+
+
+# for thing in things:
+
